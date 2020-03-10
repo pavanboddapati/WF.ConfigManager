400848013536227e0ec664db7cbe06e10b6c27bf
Pavan Boddapati <pavan.boddapati@live.com>
2020-03-09 21:32:57 -0400

    Add project files.

---
+++ ConfigData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WF.ConfigManager
+{
+    public class ConfigData
+    {
+        public List<BSCData> BSCData { get; set; }
+        public List<AppData> AppData { get; set; }
+    }
+
+    public class BSCData
+    {
+        public string ApplicationId { get; set; }
+        public string AppManager { get; set; }
+        public string BSCContact { get; set; }
+        public string PrimaryContact { get; set; }
+    }
+
+    public class AppData
+    {
+        public string AppId { get; set; }
+        public string AppName { get; set; }
+        public int CRPriority { get; set; }
+    }
+}
---
+++ WFConfigJsonParser.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WF.ConfigManager
+{
+    internal class WFConfigJsonParser
+    {
+        internal static void ParseConfigFile()
+        {
+            using (StreamReader sr = new StreamReader("WFConfig.json"))
+            {
+                string jsonString = sr.ReadToEnd();
+                var result = JsonConvert.DeserializeObject<ConfigData>(jsonString);
+            }
+        }
+    }
+}
